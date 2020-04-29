ffbd596dd42b4b05560d177a21e679a602579a43
alikemal.ocalan <alikemal.ocalan@trendyol.com>
2020-04-29 14:20:05 +0300

    Fix reformat

--- src/main/scala/com/github/alikemalocalan/Config.scala
+++ src/main/scala/com/github/alikemalocalan/Config.scala
@@ -3,10 +3,11 @@ package com.github.alikemalocalan
 import com.typesafe.config.ConfigFactory
 
 trait Config {
+  private val config = ConfigFactory.load
+  private val hostConfig = config.getConfig("http")
+
   val address: String = hostConfig.getString("interface")
   val port: Int = hostConfig.getInt("port")
   val masterCount: Int = hostConfig.getInt("masterCount")
   val workerCount: Int = hostConfig.getInt("masterCount")
-  private val config = ConfigFactory.load
-  private val hostConfig = config.getConfig("http")
 }
